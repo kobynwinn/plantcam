20bae574514d32d7b82141626944180a2f378f5c
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2023-12-12 19:10:09 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,37 +2,33 @@ from flask import Flask, render_template, Response, request, send_from_directory
 import eric
 from camera import VideoCamera
 
-pi_camera = VideoCamera(flip=False)
+pi_camera = VideoCamera(flip=False) # Sets up Pi Camera
 
-app = Flask(__name__)
-Data = {
-    "temp": 0,
-    "water": 0,
-}
-#sets up data from other files as well as libraries
+app = Flask(__name__) # Create a new Flask data set
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-#sets up html website
-def gen(camera):#sets up our camera with the camera python file
+    return render_template('index.html') # Renders a HTML file 
+
+
+def gen(): # When Called Yeilds out a Byte Array of the Current Frame from the Pi Camera
         while True:
             frame = pi_camera.get_frame()
             yield (b'--frame\r\n'
                 b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():#handles video feed
+@app.route('/video_feed') # Rerouts any http:/localhost:5000/video_feed to this file
+def video_feed():
     return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='multipart/x-mixed-replace; boundary=frame') # AnyThing from gen will update the info on http:/localhost:5000/video_feed
    
-@app.route("/Dy_update")
-def Dy_update():#handles updating the data from the eric.py folder
-     global Data
-     Data = {
-        "temp": eric.temps(),
-        "water": eric.callback(), 
-     }
-     return Data
+@app.route("/Dy_update") # Rerouts any http:/localhost:5000/Dy_update to this file
+def Dy_update():
+     return {
+        "temp": eric.temps(), # Call eric.py temps for the current Temtature 
+        "water": eric.callback(), # Calls the callback for water level from eric.py
+     } #sends it back so the website gets updated
 
 if __name__ == '__main__': #runs main
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False) #Hosts the Site and gives out 1 - 2 Ip Addresses 
